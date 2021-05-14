718099913e1c31870f8a717980cd16da488a55ce
hungnguyenpisces <hungnguyenpisces@gmail.com>
2021-05-14 20:53:13 +0700

    2.3 code optimization

--- Assignment2/2.3.cpp
+++ Assignment2/2.3.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 int main()
 {
+	while(true){
 	float a,b,c;
 	printf("\nkiem tra 3 so a b c co phai canh cua tam giac hay khong\n");
 	
@@ -16,24 +17,16 @@ int main()
 	printf("\nnhap so c: ");
 	scanf("%f",&c);
 	
-	if(("f",a)+("%f",b)>("%f",c)){
-		if(("f",a)+("%f",c)>("%f",b)){
-			if(("f",b)+("%f",c)>("%f",a)){
+	if(a+b>c && a+c>b && b+c>a){
 				float p, chuvi, dientich;
-				p=(("f",a)+("%f",b)+("%f",c))/2;
-				chuvi=("f",a)+("%f",b)+("%f",c);
-				dientich=sqrt(("%f",p)*(("%f",p)-("%f",a))*(("%f",p)-("%f",b))*(("%f",p)-("%f",c)));
+				p=(a+b+c)/2;
+				chuvi=(a+b+c);
+				dientich=sqrt(p*(p-a)*(p-b)*(p-c));
 				printf("\na b c la 3 canh cua mot tam giac\n");
 				printf("\nchu vi cua tam gia do = %f\n",chuvi);
 				printf("\ndien tich cua tam giac do = %f\n",dientich);
-			}else{
-				printf("\na b c la khong phai la 3 canh cua mot tam giac\n");
-			}
-		}else{
-			printf("\na b c la khong phai la 3 canh cua mot tam giac\n");
-		}
 	}else{
 		printf("\na b c la khong phai la 3 canh cua mot tam giac\n");
 	}
-	
+	}	
 }
