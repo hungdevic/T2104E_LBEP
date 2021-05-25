4992c8936eb1dd4bf8a4418ecc057b6abd2d5b92
hungnguyenpisces <hungnguyenpisces@gmail.com>
2021-05-25 22:05:01 +0700

    Assignment6

--- Assignment6/6.1.cpp
+++ Assignment6/6.1.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
@@ -8,8 +7,8 @@ int main()
     getchar();
     system("cls");
     do{
-        int menuChinh;
-        while (menuChinh!=1 && menuChinh!=2 && menuChinh!=3 && menuChinh!=4)
+        int menuChinh=0;
+        while (menuChinh<1 || menuChinh>4)
         {
             printf("\nCHAO MUNG QUY KHACH DEN VOI NHA HANG!!!\n\nXin moi quy khach chon theo MENU.\n");
             printf("\n1.\tMon An\n\n2.\tDo Uong\n\n3.\tThanh Toan\n\n4.\tThoat.\n\n");
@@ -23,10 +22,11 @@ int main()
 	    	case 1:
             system("cls");
                 int monAn;
-                while (monAn>7 || monAn<1)
+                while (monAn<1 || monAn>5)
                 {
                     printf("\nThuc don mon an: \n");
-                    printf("\n1.\tCom Ga\t40k\n\n2.\tCom Cho\t50k\n\n3.\tCom Lon\t30k\n\n4.\tBun\t30k\n\n5.\tPho\t30k\n\n6.\tMy\t30k\n\n7.\tQuay lai\n\n");
+                    printf("\n1.\tCom Ga\t40k\n\n2.\tBun\t30k\n\n3.\tPho\t30k");
+                    printf("\n\n4.\tMy\t30k\n\n5.\tQuay lai\n\n");
                     printf("\nMoi ban chon: ");
                     scanf("%d",&monAn);
                     system("cls");
@@ -40,29 +40,13 @@ int main()
                     }
                     if (monAn==2)
                     {
-                        printf("\nCam on quy khach da chon mon Com Cho 50k\n");
-                        thanhtoan = thanhtoan + 50;
-                        getchar();
-                        monAn=0;
-                        break;
-                    }
-                    if (monAn==3)
-                    {
-                        printf("\nCam on quy khach da chon mon Com Lon 30k\n");
-                        thanhtoan = thanhtoan + 30;
-                        getchar();
-                        monAn=0;
-                        break;
-                    }
-                    if (monAn==4)
-                    {
                         printf("\nCam on quy khach da chon mon Bun 30k\n");
                         thanhtoan = thanhtoan + 30;
                         getchar();
                         monAn=0;
                         break;
                     }
-                    if (monAn==5)
+                    if (monAn==3)
                     {
                         printf("\nCam on quy khach da chon mon Pho 30k\n");
                         thanhtoan = thanhtoan + 30;
@@ -70,7 +54,7 @@ int main()
                         monAn=0;
                         break;
                     }
-                    if (monAn==6)
+                    if (monAn==4)
                     {
                         printf("\nCam on quy khach da chon mon My 30k\n");
                         thanhtoan = thanhtoan + 30;
@@ -78,7 +62,7 @@ int main()
                         monAn=0;
                         break;
                     }
-                    if (monAn==7)
+                    if (monAn==5)
                     {
                         monAn=0;
                         break;
@@ -88,10 +72,11 @@ int main()
 		    case 2:
             system("cls");
                 int doUong;
-                while (doUong>7 || doUong<1)
+                while (doUong<1 || doUong>5)
                 {
                     printf("\nThuc don do uong: \n");
-                    printf("\n1.\tCoCaCoLa\t10k\n\n2.\tPepsi\t\t10k\n\n3.\tRedBull\t\t15k\n\n4.\tSting\t\t10k\n\n5.\tC2\t\t10k\n\n6.\tTra Da\t\tfree\n\n7.\tQuay lai\n\n");
+                    printf("\n1.\tCoCaCoLa\t10k\n\n2.\tPepsi\t\t10k\n\n3.\tRedBull\t\t15k");
+                    printf("\n\n4.\tTra Da\t\tfree\n\n5.\tQuay lai\n\n");
                     printf("\nMoi ban chon: ");
                     scanf("%d",&doUong);
                     system("cls");
@@ -121,29 +106,12 @@ int main()
                     }
                     if (doUong==4)
                     {
-                        printf("\nCam on quy khach da chon Sting 10k\n");
-                        thanhtoan = thanhtoan + 10;
+                        printf("\nCam on quy khach da chon Tra da FREE!!!\n");
                         getchar();
                         doUong=0;
                         break;
                     }
                     if (doUong==5)
-                    {
-                        printf("\nCam on quy khach da chon C2 10k\n");
-                        thanhtoan = thanhtoan + 10;
-                        getchar();
-                        doUong=0;
-                        break;
-                    }
-                    if (doUong==6)
-                    {
-                        printf("\nCam on quy khach da chon Tra da FREE!!!\n");
-                        thanhtoan = thanhtoan + 0;
-                        getchar();
-                        doUong=0;
-                        break;
-                    }
-                    if (doUong==7)
                     {
                         doUong=0;
                         break;
@@ -155,18 +123,16 @@ int main()
 		    	printf("\nThanh toan: \n");
                 if (thanhtoan==0)
                 {
-                    printf("\nXin moi quy khach chon mon truoc.\nChua mua gi ma da chon 'Thanh toan' lam sao?");
+                    printf("\nXin moi quy khach chon mon truoc.");
                 }else{
-                    printf("\nCam on quy khach da den voi nha hang!\n\nTong so tien voi nhung mon quy khach da chon la %dk.\n",thanhtoan);
+                    printf("\nCam on quy khach da den voi nha hang!\n");
+                    printf("\nTong so tien voi nhung mon quy khach da chon la %dk.\n",thanhtoan);
                 }
 		    	break;
             case 4:
             system("cls");
-                if(thanhtoan==0){
-                    printf("\nO kia! Quy khach vua moi vao nha hang thi phai an uong da chu!\nchua gi da chon 'Thoat' la sao?\n");
-                }else{
-                    printf("\nXin moi quy khach ra ban ngoi cho,\nNhan vien phuc vu se mang do ra som nhat co the.\nCam on quy khach da su dung dich vu!\nChuc quy khach ngon mieng!\n");
-                }
+                printf("\nXin moi quy khach ra ban ngoi cho,\nNhan vien se den phuc vu quy khach som nhat.");
+                printf("\nCam on quy khach da den nha hang!\nChuc quy khach ngon mieng!\n");
                 thanhtoan-=thanhtoan;
                 break;
     		default:
